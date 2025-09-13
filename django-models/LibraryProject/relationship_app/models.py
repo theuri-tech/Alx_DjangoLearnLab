70d4bdd9de9a56f5631c92eaf24c8ca965642b7b
Scott Theuri <theuriscottorg@gmail.com>
2025-09-13 03:42:10 +0300

    Add __str__ method to Author Model.

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
