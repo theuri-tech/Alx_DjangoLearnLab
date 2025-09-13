888f6c77dbf381980e0972e6b1c4c8a9e9c98636
Scott Theuri <theuriscottorg@gmail.com>
2025-09-13 07:18:02 +0300

    Update urls.py to ude views.register for checker compliance.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import register, list_books, LibraryDetailView
+from .views import  list_books, LibraryDetailView
+from . import views
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', register, name='register'),
+    path('register/', views.register, name='register'),
 ]
